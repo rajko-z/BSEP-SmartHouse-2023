a6691a53b283ff3121d82c5bc60f55f6813d245e
Raffayet <soviljnikola3@gmail.com>
2023-03-26 18:21:44 +0200

    initialized front

--- front/src/app/app-routing.module.ts
+++ front/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminDashboardComponent } from './admin/admin-dashboard/admin-dashboard.component';
+import { UserDashboardComponent } from './user/user-dashboard/user-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserDashboardComponent
+  },
+  {
+    path: 'admin',
+    component: AdminDashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
