2158860da623ab4ca955447a07d122c6e5f1a1d0
Raffayet <soviljnikola3@gmail.com>
2023-03-27 23:03:48 +0200

    created table for certificates

--- front/src/app/pages/admin/admin-certificates-page/admin-certificates-page.component.ts
+++ front/src/app/pages/admin/admin-certificates-page/admin-certificates-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface UserData {
+  firstName: string;
+  lastName: string
+}
+
+const TABLE_DATA: UserData[] = [
+  {firstName: "Djura", lastName: "Djuric"},
+  {firstName: "Mika", lastName: "Djuric"},
+  {firstName: "Pera", lastName: "Djuric"},
+]
+
 @Component({
   selector: 'app-admin-certificates-page',
   templateUrl: './admin-certificates-page.component.html',
   styleUrls: ['./admin-certificates-page.component.scss']
 })
 export class AdminCertificatesPageComponent {
-  displayedColumns = ['firstName', 'lastName', 'verifyButton'];
+  displayedColumns = ['firstName', 'lastName', 'verifyButton', 'cancelButton'];
+  dataSource = TABLE_DATA;
 }
