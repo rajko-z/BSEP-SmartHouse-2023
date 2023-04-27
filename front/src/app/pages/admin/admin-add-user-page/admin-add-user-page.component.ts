f0ff0ea6e14eb76c1d1dc73dc992eea2b9e6ca4b
Raffayet <soviljnikola3@gmail.com>
2023-04-27 19:12:39 +0200

    initial add user form

--- front/src/app/pages/admin/admin-add-user-page/admin-add-user-page.component.ts
+++ front/src/app/pages/admin/admin-add-user-page/admin-add-user-page.component.ts
@@ -1,62 +1,76 @@
-import { Component } from '@angular/core';
-import {FormControl, FormGroup, Validators, AbstractControl, ValidatorFn, ValidationErrors} from '@angular/forms';
-import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
-import { UserService } from 'src/app/services/user/user.service';
+  import { Component } from '@angular/core';
+  import {FormControl, FormGroup, Validators, AbstractControl, ValidatorFn, ValidationErrors, FormArray} from '@angular/forms';
+  import { Router } from '@angular/router';
+  import { ToastrService } from 'ngx-toastr';
+import { FacilityData } from 'src/app/model/facilityData';
+  import { UserService } from 'src/app/services/user/user.service';
 
 
-@Component({
-  selector: 'app-admin-add-user-page',
-  templateUrl: './admin-add-user-page.component.html',
-  styleUrls: ['./admin-add-user-page.component.scss']
-})
-export class AdminAddUserPageComponent {
+  @Component({
+    selector: 'app-admin-add-user-page',
+    templateUrl: './admin-add-user-page.component.html',
+    styleUrls: ['./admin-add-user-page.component.scss']
+  })
+  export class AdminAddUserPageComponent {
 
-  form: FormGroup;
+    userDataForm: FormGroup;
 
-  constructor(private userService:UserService, private toastrService:ToastrService, private router:Router) {}
+    facilities: FacilityData[] = [
+      new FacilityData('', 'House')
+    ]
 
-  ngOnInit(){
-    this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      firstName: new FormControl('',[Validators.required]),
-      lastName: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.pattern("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}")]),
-      confirmPassword: new FormControl('', [Validators.required]),
-    }, { validators: this.validatePassword })
-  }
+    facilityTypes: string[] = ['House', 'Apartment', 'Cottage'];
 
-  public validatePassword: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
-    const password = control.get('password');
-    const confirmPassword = control.get('confirmPassword');
-  
-    if(password && confirmPassword && password.value !== confirmPassword.value){
-      confirmPassword.setErrors({notEquivalent: true});
+    constructor(private userService:UserService, private toastrService:ToastrService, private router:Router) {}
+
+    ngOnInit(){
+      this.userDataForm = new FormGroup({
+        email: new FormControl('', [Validators.required, Validators.email]),
+        firstName: new FormControl('',[Validators.required]),
+        lastName: new FormControl('', [Validators.required]),
+        password: new FormControl('', [Validators.required, Validators.pattern("(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}")]),
+        confirmPassword: new FormControl('', [Validators.required]),
+      }, { validators: this.validatePassword })
     }
-    else if(password && confirmPassword && !confirmPassword.errors?.['required']){
-      confirmPassword.setErrors(null);
+
+    public validatePassword: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+      const password = control.get('password');
+      const confirmPassword = control.get('confirmPassword');
+    
+      if(password && confirmPassword && password.value !== confirmPassword.value){
+        confirmPassword.setErrors({notEquivalent: true});
+      }
+      else if(password && confirmPassword && !confirmPassword.errors?.['required']){
+        confirmPassword.setErrors(null);
+      }
+    
+      return password && confirmPassword && password.value !== confirmPassword.value ? { notMatchingPasswords: true } : null;
     }
-  
-    return password && confirmPassword && password.value !== confirmPassword.value ? { notMatchingPasswords: true } : null;
-  }
 
-  get firstName(){
-    return this.form.get("firstName");
-  }
-  get lastName(){
-    return this.form.get("lastName");
-  }
-  get email(){
-    return this.form.get("email");
-  }
-  get password(){
-    return this.form.get("password");
-  }
-  get confirmPassword(){
-    return this.form.get("confirmPassword");
-  }
+    get firstName(){
+      return this.userDataForm.get("firstName");
+    }
+    get lastName(){
+      return this.userDataForm.get("lastName");
+    }
+    get email(){
+      return this.userDataForm.get("email");
+    }
+    get password(){
+      return this.userDataForm.get("password");
+    }
+    get confirmPassword(){
+      return this.userDataForm.get("confirmPassword");
+    }
 
-  public onSubmit(): void {
-    console.log(this.form);
+    addFacility() {
+      this.facilities.push(new FacilityData('', 'House'));
+      const facility = new FormGroup({
+        name: new FormControl('', [Validators.required])
+      });
+    } 
+
+    public onSubmit(): void {
+      console.log(this.userDataForm);
+    }
   }
-}
