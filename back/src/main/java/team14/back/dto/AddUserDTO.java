fb916af525c8df5d0150801f98762dd3c30afdca
Raffayet <soviljnikola3@gmail.com>
2023-05-02 17:25:16 +0200

    xss checkpooit

--- back/src/main/java/team14/back/dto/AddUserDTO.java
+++ back/src/main/java/team14/back/dto/AddUserDTO.java
@@ -7,6 +7,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import team14.back.model.Facility;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @AllArgsConstructor
@@ -14,8 +17,13 @@ import java.util.List;
 @Getter
 @Setter
 public class AddUserDTO {
+    @Email
     private String email;
+    @NotBlank
+    @Max(256)
     private String firstName;
+    @NotBlank
+    @Max(256)
     private String lastName;
     private String password;
     private String confirmPassword;
--- back/src/main/java/team14/back/dto/FacilityDTO.java
+++ back/src/main/java/team14/back/dto/FacilityDTO.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import team14.back.model.Facility;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 import java.util.List;
 
 @AllArgsConstructor
@@ -13,6 +16,11 @@ import java.util.List;
 @Getter
 @Setter
 public class FacilityDTO {
+    @NotBlank
+    @Max(256)
     private String name;
+    @NotBlank
+    @Max(256)
+    @Pattern(regexp = "House|Apartment|Cottage")
     private String facilityType;
 }
